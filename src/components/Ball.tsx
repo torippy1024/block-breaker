478dfcace76be56c7e71521c0b95cf08e249f8e4
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-16 00:04:36 +0900

    useIntervalの導入

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import { useState, useEffect } from "react";
+import useInterval from "./useInterval";
 
 type BallType = {
   width: number;
@@ -12,31 +13,25 @@ export type Vector2Type = {
 };
 
 const Ball = ({width = 100, height = 120}: BallType) => {
-  const posNow: Vector2Type = {x: 0, y: 0};
-  const vector: Vector2Type = {x: 1, y: 1};
-  const [pos, setPos] = useState<Vector2Type>(posNow);
+  const [pos, setPos] = useState<Vector2Type>({x: 0, y: 0});
+  const [vector, setVector] = useState<Vector2Type>({x: 1, y: 1});
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (pos.x <= 0) {
-        vector.x = 1;
-      }
-      if (pos.x >= width) {
-        vector.x = -1;
-      }
-      if (pos.y <= 0) {
-        vector.y = 1;
-      }
-      if (pos.y >= height) {
-        vector.y = -1;
-      }
-      posNow.x = pos.x + vector.x;
-      posNow.y = pos.y + vector.y;
-      setPos({x: posNow.x, y: posNow.y});
-      console.log(posNow);
-    }, 10);
-    return () => clearInterval(interval);
-  }, []);
+  useInterval(() => {
+    console.log(pos, vector);
+    if (pos.x <= 0) {
+      setVector(vector => ({...vector, x: 1}));
+    }
+    if (pos.x >= width) {
+      setVector(vector => ({...vector, x: -1}));
+    }
+    if (pos.y <= 0) {
+      setVector(vector => ({...vector, y: 1}));
+    }
+    if (pos.y >= height) {
+      setVector(vector => ({...vector, y: -1}));
+    }
+    setPos(pos => ({x: pos.x + vector.x, y: pos.y + vector.y}));
+  }, 10);
 
   return (
     <div>
